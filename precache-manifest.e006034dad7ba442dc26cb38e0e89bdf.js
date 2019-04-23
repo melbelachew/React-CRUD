13b5e3a922e587e7e5c87300c313264680b2582d
Mel Belachew <melbelachew@gmail.com>
2019-04-23 12:36:19 -0400

    Updates

---
+++ precache-manifest.e006034dad7ba442dc26cb38e0e89bdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6c1bcbcef5da731b76f",
+    "url": "/React-CRUD/static/css/main.7aa27fca.chunk.css"
+  },
+  {
+    "revision": "b6c1bcbcef5da731b76f",
+    "url": "/React-CRUD/static/js/main.d2740e60.chunk.js"
+  },
+  {
+    "revision": "1d591331fece7c8b061e",
+    "url": "/React-CRUD/static/js/runtime~main.d49f3254.js"
+  },
+  {
+    "revision": "45f2e7d1c032b406779e",
+    "url": "/React-CRUD/static/js/2.991b768e.chunk.js"
+  },
+  {
+    "revision": "168e928df36e1e16a43d8e7e71e3a6fc",
+    "url": "/React-CRUD/index.html"
+  }
+];
